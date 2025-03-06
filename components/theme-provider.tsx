d2040ac20e3df4ddcbf99b6b5dfa2c10dc059219
wengstA <1164329581@qq.com>
2025-03-06 18:04:50 +0800

    修复: 更新 next-themes 类型导入路径

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,8 +2,18 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="light" 
+      enableSystem={true} 
+      disableTransitionOnChange={false}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
